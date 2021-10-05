9ac3df48bdeebc6911e75adeb9779bb95a6927a9
Bing Jheung <bing@ososo.io>
2021-10-05 19:35:55 +0800

    Rename deduplicated() to uniqued()

--- Sources/JyutBot/JyutpingProvider.swift
+++ Sources/JyutBot/JyutpingProvider.swift
@@ -23,12 +23,16 @@ struct JyutpingProvider {
                         }
                 }
                 sqlite3_finalize(queryStatement)
-                return jyutpings.deduplicated()
+                return jyutpings.uniqued()
         }
 }
 
 private extension Array where Element: Hashable {
-        func deduplicated() -> [Element] {
+
+        /// Returns a new Array with the unique elements of this Array, in the order of the first occurrence of each unique element.
+        /// - Returns: A new Array with only the unique elements of this Array.
+        /// - Complexity: O(*n*), where *n* is the length of the Array.
+        func uniqued() -> [Element] {
                 var set: Set<Element> = Set<Element>()
                 return filter { set.insert($0).inserted }
         }
